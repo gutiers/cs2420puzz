0486e409abf15467f9a630c40f7699967f4964d2
sjgv <salvador.gvz@gmail.com>
2017-03-17 23:47:40 -0600

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,242 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Board {
+	final private int[] board;
+	private int hamming;
+	private int manhattan;
+	private int rowSize;
+	private int colSize;
+	private int zedRow;
+	private int zedIndex;
+	private int inversions;
+	private int N;
+	private int[] temp;
+	//private int[][] toTwoArr;
+	public Board(int[][] blocks){
+		this.rowSize = blocks.length; //notice blocks.length not blocks[0] for row
+		this.colSize = blocks[1].length;
+		this.N = rowSize * colSize;
+		this.board = new int[N];
+		//1D CONVERSION***************************************************************************************
+		for(int i = 0; i < rowSize; i++){
+			for(int j = 0; j < colSize; j++){
+				int val = blocks[i][j];
+				//System.out.println("i: " + i + " j: " + j + " Val = " + val);
+				int index = (colSize * i) + j;
+				this.board[index] = val;
+				//HEURISTICS**********************************************************************************
+				if(val != (index + 1)){
+					this.hamming += 1;
+					if(val == 0){
+						this.zedRow = i;
+						this.zedIndex = index;
+					}
+					else{
+						int oneDDest = (int)val -1;
+						int row = oneDDest/colSize;
+						int col = oneDDest%colSize;
+						this.manhattan += (Math.abs(row - i) + Math.abs(col-j)) ;
+					}
+				}
+			}	
+		}
+	}
+	
+	public int boardSize(){
+		return board.length;
+	}
+	
+	public int hamming(){
+		return hamming - 1; //take away the zed's position
+	}
+	public int manhattan(){
+		return manhattan;
+	}
+	public boolean isGoal(){
+		return manhattan  == 0;
+	}
+	public boolean isSolvable(){
+		
+		int thisParity = (rowSize - 1) % 2;	//parity is inversions + row of zed all % 2
+		int lo = 0;
+		int hi = N-1;	//our high is with respect to the new array we create without zero, thus n-1
+		temp = new int[hi];
+		
+		for(int i = 0; i < temp.length; i++){  //get rid of zed for inversions
+			if(board[i] != 0) temp[i] = board[i];
+		}
+		inversions= mergeSort(temp,lo,hi);
+		System.out.println("inversions = "+inversions);
+		return (zedRow + inversions)%2 == thisParity;
+	}
+	
+	public boolean equals(Object board){
+		//check for self comparison
+		//null case?
+		if(board == null) throw new java.lang.NullPointerException();
+		if(this == board) return true;
+		if(!(board instanceof Board)) return false;
+		Board that = (Board) board;
+		if(this.zedIndex == that.zedIndex && this.inversions == that.inversions && this.boardSize() == that.boardSize() && 
+				this.manhattan == that.manhattan){
+			for(int i = 0; i < N; i++){
+				if(this.board[i] != that.board[i]) return false;
+			} return true;
+			//instead of checking every square i'll do diagonals and a ratio of random sqrs POSSIBLE PERFORMANCE IMPROVEMENTS
+		}
+		return false;
+	}
+	public Iterable<Board> neighbors(){
+		ArrayList<Board> boardList = new ArrayList<Board>();
+		//if !on edge
+		if(zedIndex > colSize -1 && zedIndex < N-colSize && zedIndex%colSize != (colSize-1) && zedIndex%colSize != 0){
+			//up
+			boardList.add(nborUp());
+			//start off
+			boardList.add(nborDown());
+			//start off
+			boardList.add(nborL());
+			//start off
+			boardList.add(nborR());
+		}
+		//if on top
+		else if(zedIndex <= colSize-1){
+			boardList.add(nborDown());
+			if(zedIndex%colSize != (colSize-1)) boardList.add(nborR());
+			if(zedIndex%colSize != 0) boardList.add(nborL());
+		}
+		//if on bottom
+		else if(zedIndex >= N-colSize){
+			boardList.add(nborUp());
+			if(zedIndex%colSize != (colSize-1)) boardList.add(nborR());
+			if(zedIndex%colSize != 0) boardList.add(nborL());
+		}
+		//if on right margin
+		else if(zedIndex%colSize == (colSize-1)){
+			boardList.add(nborL());
+			if(zedIndex > colSize -1) boardList.add(nborUp());
+			if(zedIndex < N-colSize) boardList.add(nborDown());
+		}//if on left margin
+		else if(zedIndex%colSize == 0){
+			boardList.add(nborR());
+			if(zedIndex > colSize -1) boardList.add(nborUp());
+			if(zedIndex < N-colSize) boardList.add(nborDown());
+		}
+		return boardList;
+		/**
+		return new Iterable<Board>(){
+
+			@Override
+			public Iterator<Board> iterator() {
+				// TODO Auto-generated method stub
+				return new Iterator<Board>(){
+					private int position;
+					
+
+					@Override
+					public boolean hasNext() {
+						return false;
+					}
+
+					@Override
+					public Board next() {
+						// TODO Auto-generated method stub
+						return null;
+					}
+
+					@Override
+					public void remove() {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				};
+			}
+			
+		};
+		**/
+		
+	}
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("\n");
+		sb.append(colSize);
+		sb.append("\n");
+		for(int i = 0; i < N; i++){
+			if(i%colSize == colSize-1){
+				sb.append(String.format("%3d",board[i]));
+				sb.append("\n");
+			}
+			else
+				sb.append(String.format("%3d",board[i]));
+		}
+		
+		return sb.toString();
+	}
+	
+	//************************PRIVATE METHODS*************************************
+	
+	private int mergeSort(int[] a, int lo, int hi){
+		if (lo == hi -1) return 0; //base case
+		int mid = lo + (hi - lo)/2;
+		return mergeSort(a,lo, mid) + mergeSort(a,mid,hi) + merge(a,lo,mid,hi); 
+	}
+	
+	private int merge(int[] a, int lo, int mid, int hi){
+		int count = 0;
+		
+		for(int i = lo, lp = lo, hp = mid; i <  hi; i++){
+			if(hp >= hi || lp < mid && a[lp] <= a[hp]) lp++;
+			else{
+				if (a[lp] > a[hp]){
+					count += (mid-lp);
+					hp++;
+				}
+			}
+		}return count;
+	}
+	
+	private int[][] toTwoD(int[] arr){
+		int[][] toTwoArr = new int[rowSize][colSize];
+		for (int i = 0; i < N; i++){
+			int row = i/colSize;
+			int col = i%colSize;
+			toTwoArr[row][col] = arr[i];
+			}
+	return toTwoArr; //this way I only allocate memory for 1 2dimensional array that gets repopulated by the neighbors. methinks 
+	}
+	
+	private Board nborUp(){
+		int[] neighbor = (int[])board.clone();
+		int swapVal = neighbor[zedIndex-colSize];
+		neighbor[zedIndex] = swapVal;
+		neighbor[zedIndex-colSize] = 0;
+		return new Board(toTwoD(neighbor));
+	}
+	
+	private Board nborDown(){
+		int[] neighbor = (int[])board.clone();
+		int swapVal = neighbor[zedIndex+colSize];
+		neighbor[zedIndex] = swapVal;
+		neighbor[zedIndex+colSize] = 0;
+		return new Board(toTwoD(neighbor));
+	}
+	
+	private Board nborL(){
+		int[] neighbor = (int[])board.clone();
+		int swapVal = neighbor[zedIndex-1];
+		neighbor[zedIndex] = swapVal;
+		neighbor[zedIndex-1] = 0;
+		return new Board(toTwoD(neighbor));
+	}
+	
+	private Board nborR(){
+		int[] neighbor = (int[])board.clone();
+		int swapVal = neighbor[zedIndex+1];
+		neighbor[zedIndex] = swapVal;
+		neighbor[zedIndex+1] = 0;
+		return new Board(toTwoD(neighbor));	 
+	}
+}
---
+++ Solver.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+
+public class Solver {
+	private int moves = 0;
+	private SearchNode solution;
+	public Solver(Board initial){
+		SearchNode sn = new SearchNode(initial,moves,null);
+		SearchNode husk;
+		MinPQ<SearchNode> pq = new MinPQ(sn.byManhattan());
+		pq.insert(sn);
+		while(!pq.isEmpty()){
+			sn = pq.delMin();
+			//is it goal?
+			if(sn.board.isGoal()){
+				solution = sn;
+				solution();
+				break;
+			}
+			//is it same?
+			else if(sn.previous == null || !sn.board.equals(sn.previous.board)){
+				//expand
+				for(Board el : sn.board.neighbors()){
+					husk = new SearchNode(el,moves,sn);
+					pq.insert(husk);
+				}
+				moves++;
+			}
+		}
+		
+	}
+	public int moves(){
+		return moves;
+	}
+	public Iterable<Board> solution(){
+		ArrayList<Board> myList = new ArrayList<>();
+		while(solution.previous != null){
+			myList.add(solution.board);
+			solution = solution.previous;
+		}
+		for(Board el:myList) System.out.println(el);
+		return myList;
+	}
+	
+	private class SearchNode implements Comparable<SearchNode>{
+		public int moves;
+		public Board board;
+		public SearchNode previous;
+		public int manhattan;
+		public int hamming;
+		public SearchNode(Board board, int level,SearchNode previous){
+			this.moves = level;
+			this.manhattan = board.manhattan();
+			this.hamming = board.hamming();
+			this.previous = previous;
+			this.board = board;
+		}
+		public Comparator<SearchNode> byManhattan() {
+	        return new Comparator<SearchNode>(){
+	            public int compare(SearchNode sn1, SearchNode sn2) {
+	            	if(sn1.manhattan + moves == sn2.manhattan+moves) return 0;
+	    			else if(sn1.manhattan + moves > sn2.manhattan+moves) return 1;
+	    			else return -1; 
+	            } 
+	        };
+	        
+	    }
+		public Comparator<SearchNode> byHamming(){
+			return new Comparator<SearchNode>(){
+				public int compare(SearchNode sn1, SearchNode sn2){
+					if(sn1.hamming + moves == sn2.hamming + moves) return 0;
+					else if(sn1.hamming + moves > sn2.hamming) return 1;
+					else return -1;
+				}
+			};
+		}
+		@Override
+		public int compareTo(SearchNode arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+	}
+
+	public static void main(String[] args) {
+
+	    // create initial board from file
+	    //In in = new In(args[0]);
+		In in = new In("src/test");
+	    int N = in.readInt();
+	    int[][] blocks = new int[N][N];
+	    for (int i = 0; i < N; i++)
+	        for (int j = 0; j < N; j++)
+	            blocks[i][j] = in.readInt();
+	    Board initial = new Board(blocks);
+
+	    // check if puzzle is solvable; if so, solve it and output solution
+	    if (initial.isSolvable()) {
+	        Solver solver = new Solver(initial);
+	        StdOut.println("Minimum number of moves = " + solver.moves());
+	        //System.out.println(solver.solution());
+	        for (Board board : solver.solution())
+	            StdOut.println(board);
+	    }
+
+	    // if not, report unsolvable
+	    else {
+	        StdOut.println("Unsolvable puzzle");
+	    }
+	}
+	
+}
